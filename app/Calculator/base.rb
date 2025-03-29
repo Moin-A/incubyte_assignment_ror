c9daa535fefbd178570b3b917f4168739725852c
Moin-A <moinqa6@gmail.com>
2025-03-29 19:32:07 +0530

    deleted some commond methods to base class

--- app/Calculator/base.rb
+++ app/Calculator/base.rb
@@ -5,5 +5,13 @@ module Calculator
   class CalculationError < StandardError; end
 
   class Base
+    def initialize(input)
+      @input = input
+      parse_input_array if input.is_a?(String)
+    end
+
+    def parse_input_array
+      @parsed_input_array ||= @input.split(/[\n,]/).map(&:to_i)
+    end
   end
 end
