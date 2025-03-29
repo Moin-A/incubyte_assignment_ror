42d7678ec28755cacc624b14f29d307e4d203898
Moin-A <moinqa6@gmail.com>
2025-03-29 14:28:04 +0530

    added a calculator.rb file to group modules and classes together

---
+++ app/calculator.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+require_relative 'calculator/base'
+require_relative 'calculator/string_calculator'
+# The Calculator module serves as a namespace for all calculator-related classes.
+# It groups functionality for performing various types of calculations.
+module Calculator
+end
