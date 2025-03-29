8759e220eec68e255575606d95fd6a90765f7e89
Moin-A <moinqa6@gmail.com>
2025-03-29 14:29:49 +0530

    added a basic set of specs

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../app/calculator'
+
+RSpec.describe Calculator::StringCalculator do
+  it 'returns 0 for an empty string' do
+  end
+
+  it 'returns the number itself when a single number is provided' do
+  end
+
+  it 'returns the sum of multiple numbers separated by commas' do
+  end
+
+  it 'handles new lines as delimiters along with commas' do
+  end
+
+  it 'supports custom delimiters specified at the beginning of the string' do
+  end
+
+  it 'throws an exception when a negative number is provided' do
+  end
+
+  it 'throws an exception listing all negative numbers if multiple are provided' do
+  end
+end
