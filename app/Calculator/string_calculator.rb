3be9341ff7b00f7fabd6860d52513a159d81e275
Moin-A <moinqa6@gmail.com>
2025-03-29 20:01:02 +0530

    added a exception for negetive number and reorganised the code

--- app/Calculator/string_calculator.rb
+++ app/Calculator/string_calculator.rb
@@ -6,14 +6,18 @@ module Calculator
   class StringCalculator < Base
     attr_reader :input, :parsed_input_array
 
-    def initialize(input)
-      super
-    end  
-
     def add
       return 0 if @input.empty?
-      # raise CalculationError, 'negatives not allowed' if numbers.any?(&:negative?)
+
+      raise CalculationError, "negatives not allowed: #{negative_numbers.join(', ')}" if negative_numbers.any?
+
       @parsed_input_array.sum
+    end
+
+    private
+
+    def negative_numbers
+      @negative_numbers ||= @parsed_input_array.select(&:negative?)
     end
   end
 end
