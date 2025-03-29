fa6dca62f7b877529e4e900ea2cae28fdb1b5c0c
Moin-A <moinqa6@gmail.com>
2025-03-29 20:18:08 +0530

    adde spec for custome delimiters with negetive number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -48,6 +48,9 @@ RSpec.describe Calculator::StringCalculator do
     end
   end
 
-  it 'throws an exception listing all negative numbers if multiple are provided' do
+  context 'when the string contains multiple negative numbers with custom delimiters' do
+    let(:string) { ';1;-2;-3;3' }
+    it 'throws an exception listing all negative numbers if multiple are provided' do
+    end
   end
 end
